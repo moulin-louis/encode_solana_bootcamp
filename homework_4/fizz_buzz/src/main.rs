77c07011b1ad106a983f4516949e57bc8794d113
moulin-louis <louis.moulin@outlook.fr>
2023-12-07 15:27:49 +0100

    cleanup repo and done homework 4

--- homework_4/fizz_buzz/src/main.rs
+++ homework_4/fizz_buzz/src/main.rs
@@ -1,16 +1,20 @@
 fn fizz_buzz() {
+    let mut result = 0;
     for idx in 0..=301 {
-        if idx % 3  == 0{
-            print!("fizz");
-            if idx % 5 == 0 {
-                print!(" buzz");
-            }
-        }
-        if idx % 5 == 0 {
-            print!("buzz");
-        }
-        
+        let fizz = idx % 3 == 0;
+        let buzz = idx % 5 == 0;
+
+        match (fizz, buzz) {
+            (true, true) => {
+                result += 1;
+                println!("{}","fizz buzz")
+            },
+            (true, false) => println!("{}","fizz"),
+            (false, true) => println!("{}","buzz"),
+            (false, false) => println!("{}",idx),
+        };
     }
+    println!("Number of 'fizz buzz': {}", result);
 }
 
 fn main() {
